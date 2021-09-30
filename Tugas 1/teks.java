d58baec99e7a275f532f5f76c164c8ddda991b4c
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-09-30 20:33:48 +0700

    Create teks.java

---
+++ Tugas 1/teks.java
@@ -0,0 +1,12 @@
+package Tugas1;
+class Teks{
+public static void main(String[] args) {
+    teks();
+}
+
+public static void teks(){
+    System.out.println("Belajar JAVA");
+    System.out.println("Sangat mudah sekali");
+    System.out.println("dan sangat menyenangkan");
+}
+}
