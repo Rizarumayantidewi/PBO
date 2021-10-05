22727fceb689e97fcd66626ed4530f27072e66ea
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2021-10-05 14:49:24 +0700

    Add files via upload

---
+++ Tugas 2/Ternary.java
@@ -0,0 +1,12 @@
+package Tugas2;
+public class Ternary {
+    public static void main(String[] args) {
+        int uang = 50000;
+        int harga = 55000;
+
+        String result = harga > uang ? "Uang Anda Tidak Cukup untuk membeli mainan" : "uang anda cukup dan anda dapat membeli mainan";
+        System.out.println(result);
+
+    }
+    
+}
